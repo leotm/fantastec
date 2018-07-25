26988ff3cfe0b15a68bea31d5c0c6e7a42852a98
Leo <leo.m@hotmail.co.uk>
2018-07-25 23:08:37 +0100

    Update server
    Add active keys, so that an active event can change to a purple circle.

--- src/server/app.js
+++ src/server/app.js
@@ -7,37 +7,44 @@ app.get('/events', (req, res) => {
       {
         minute: 12,
         type: 'booking',
-        text: 'Dier goes into the book in what is the first yellow card of the game'
+        text: 'Dier goes into the book in what is the first yellow card of the game',
+        active: 0
       },
       {
         minute: 14,
         type: 'commentary',
-        text: 'Kane is desparate to catch Liverpool\'s Salah in the race for the Golden Boot, but he won\'t do it like that - the England man has just hammered a wild shot from range well off target.'
+        text: 'Kane is desparate to catch Liverpool\'s Salah in the race for the Golden Boot, but he won\'t do it like that - the England man has just hammered a wild shot from range well off target.',
+        active: 0
       },
       {
         minute: 17,
         type: 'booking',
-        text: 'Now Alli gets a booking as he checks Sterling and the visitors have a free-kick.'
+        text: 'Now Alli gets a booking as he checks Sterling and the visitors have a free-kick.',
+        active: 0
       },
       {
         minute: 21,
         type: 'goal',
-        text: 'AND THERE IS THE GOAL! No mistake from Kane, as - after initially missing Son\'s cross - he gets back up off the turf to cushion in Trippier\'s pinpoint cut-back.'
+        text: 'AND THERE IS THE GOAL! No mistake from Kane, as - after initially missing Son\'s cross - he gets back up off the turf to cushion in Trippier\'s pinpoint cut-back.',
+        active: 0
       },
       {
         minute: 34,
         type: 'commentary',
-        text: 'Kane is desparate to catch Liverpool\'s Salah in the race for the Golden Boot, but he won\'t do it like that - the England man has just hammered a wild shot from range well off target.'
+        text: 'Kane is desparate to catch Liverpool\'s Salah in the race for the Golden Boot, but he won\'t do it like that - the England man has just hammered a wild shot from range well off target.',
+        active: 0
       },
       {
         minute: 42,
         type: 'commentary',
-        text: 'Kane is desparate to catch Liverpool\'s Salah in the race for the Golden Boot, but he won\'t do it like that - the England man has just hammered a wild shot from range well off target.'
+        text: 'Kane is desparate to catch Liverpool\'s Salah in the race for the Golden Boot, but he won\'t do it like that - the England man has just hammered a wild shot from range well off target.',
+        active: 0
       },
       {
         minute: 46,
         type: 'commentary',
-        text: 'Kane is desparate to catch Liverpool\'s Salah in the race for the Golden Boot, but he won\'t do it like that - the England man has just hammered a wild shot from range well off target.'
+        text: 'Kane is desparate to catch Liverpool\'s Salah in the race for the Golden Boot, but he won\'t do it like that - the England man has just hammered a wild shot from range well off target.',
+        active: 0
       }
     ],
   }));
